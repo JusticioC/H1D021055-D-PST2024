cf07521ddb13388b4d6d9f2d36d49a846e7cc28d
Justicio Caesario <106458413+JusticioC@users.noreply.github.com>
2024-04-30 11:17:17 +0700

    Pertemuan 5 Praktikum PST

---
+++ ServoMotor/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo myservo;
+
+void setup() {
+  myservo.attach(D4);
+  
+}
+
+void loop() {
+  myservo.write(90);
+  for(int pos = 0; pos <= 180; pos += 30){
+    myservo.write(pos);
+    delay(1);
+  }
+  for(int pos = 180; pos >= 0; pos -= 30){
+    myservo.write(pos);
+    delay(1);
+  }
+}
